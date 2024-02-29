641505a783c4021091015fd3c541d6e18db6ecd2
NikitaGrishenko <146408475+NikitaGrishenko@users.noreply.github.com>
2024-02-29 10:59:40 +1000

    Add files via upload

---
+++ FunPay_Grishenko/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FunPay_Grishenko
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            AppConnect.model0db = new Entities1();
+            AppFrame.FrameMain = FrmMain;
+            FrmMain.Navigate(new Page1());
+        }
+
+        
+
+        private void BtnEdit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
